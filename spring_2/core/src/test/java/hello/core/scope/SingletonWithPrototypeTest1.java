1e8e7cd7daed644bf8de2045d48e6fc2bb0cf663
gyqls980 <hyobin0715@kookmin.ac.kr>
2021-01-09 19:38:05 +0900

    use ObjectProvider for prototype bean

--- spring_2/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ spring_2/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -30,17 +31,14 @@ public class SingletonWithPrototypeTest1 {
         assertThat(count1).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
     @Scope("singleton")
     static class ClientBean {
-        // singleton : 생성시점에 주입 = ClientBean을 새로 생성해도 동일한 PrototypeBean이 존재
-        private final PrototypeBean prototypeBean;
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
