ef1a88a55707db7ce0e663782373639998010736
gyqls980 <hyobin0715@kookmin.ac.kr>
2021-01-09 19:24:18 +0900

    singleton, prototype bean

---
+++ spring_2/core/src/test/java/hello/core/scope/SigletonTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SigletonTest {
+    @Test
+    public void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        // SingletonBean.class -> 자동으로 컴포넌트 스캔으로 등록이 된다.
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+        ac.close(); //종료
+    }
+
+    // 싱글톤 빈
+    @Scope("singleton") // 생략 가능
+   static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
