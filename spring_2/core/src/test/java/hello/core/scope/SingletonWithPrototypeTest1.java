c652d0276766afd7923454a60fd5e88c6970cfb8
gyqls980 <hyobin0715@kookmin.ac.kr>
2021-01-09 19:52:45 +0900

    use Provider for prototype bean

--- spring_2/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ spring_2/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,10 +36,11 @@ public class SingletonWithPrototypeTest1 {
     }
     @Scope("singleton")
     static class ClientBean {
+        //implementation 'javax.inject:javax.inject:1' gradle 추가 필수
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> provider;
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = provider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
